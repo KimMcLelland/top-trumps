b4514565116f217e189f64e545529af45d8d263d
Kim <faeriekim3@gmail.com>
2021-04-28 13:46:44 +0100

    added class and objects

---
+++ script.js
@@ -0,0 +1,37 @@
+class card {
+    constructor (name, strength) {
+        this.name = name;
+        this.strength = strength
+    }
+}
+
+let card1 = new card("orc", 12)
+let card2 = new card("goblin", 9)
+let card3 = new card("troll", 17)
+let card4 = new card("pixie", 5)
+let card5 = new card("elf", 10)
+let card6 = new card("dwarf", 13)
+let card7 = new card("giant", 26)
+let card8 = new card("swamp creature", 18)
+let card9 = new card("wyvern", 15)
+let card10 = new card("griffon", 16)
+let card11 = new card("dragon", 30)
+let card12 = new card("zombie", 10)
+let card13 = new card("skeleton", 10)
+let card14 = new card("ghoul", 11)
+let card15 = new card("wraith", 14)
+let card16 = new card("gargoyle", 17)
+let card17 = new card("vampire", 19)
+let card18 = new card("werewolf", 18)
+let card19 = new card("golem", 24)
+let card20 = new card("demon", 21)
+let card21 = new card("wolf", 12)
+let card22 = new card("giant spider", 13)
+let card23 = new card("eagle", 12)
+let card24 = new card("tiger", 14)
+let card25 = new card("snake", 9)
+let card26 = new card("fire elemental", 15)
+let card27 = new card("water elemental", 15)
+let card28 = new card("air elemental", 12)
+let card29 = new card("earth elemental", 20)
+let card30 = new card("beholder", 23)

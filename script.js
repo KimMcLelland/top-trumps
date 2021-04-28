6ad6c9657de0c75344761b5bb4ee7860727a266e
Kim <faeriekim3@gmail.com>
2021-04-28 17:30:00 +0100

    script.js

--- script.js
+++ script.js
@@ -35,3 +35,219 @@ let card27 = new card("water elemental", 15)
 let card28 = new card("air elemental", 12)
 let card29 = new card("earth elemental", 20)
 let card30 = new card("beholder", 23)
+
+
+
+
+const pickCardOne = () => {
+    let cardChoice1 = prompt("pick your first card")
+    if (cardChoice1 === `${card1.name}`) {
+        return card1
+    }
+    else if (cardChoice1 === `${card2.name}`) {
+        return card2
+    }
+    else if (cardChoice1 === `${card3.name}`) {
+        return card3
+    }
+    else if (cardChoice1 === `${card4.name}`) {
+        return card4
+    }
+    else if (cardChoice1 === `${card5.name}`) {
+        return card5
+    }
+    else if (cardChoice1 === `${card6.name}`) {
+        return card6
+    }
+    else if (cardChoice1 === `${card7.name}`) {
+        return card7
+    }
+    else if (cardChoice1 === `${card8.name}`) {
+        return card8
+    }
+    else if (cardChoice1 === `${card9.name}`) {
+        return card9
+    }
+    else if (cardChoice1 === `${card10.name}`) {
+        return card10
+    }
+    else if (cardChoice1 === `${card11.name}`) {
+        return card11
+    }
+    else if (cardChoice1 === `${card12.name}`) {
+        return card12
+    }
+    else if (cardChoice1 === `${card13.name}`) {
+        return card13
+    }
+    else if (cardChoice1 === `${card14.name}`) {
+        return card14
+    }
+    else if (cardChoice1 === `${card15.name}`) {
+        return card15
+    }
+    else if (cardChoice1 === `${card16.name}`) {
+        return card16
+    }
+    else if (cardChoice1 === `${card17.name}`) {
+        return card17
+    }
+    else if (cardChoice1 === `${card18.name}`) {
+        return card18
+    }
+    else if (cardChoice1 === `${card19.name}`) {
+        return card19
+    }
+    else if (cardChoice1 === `${card20.name}`) {
+        return card20
+    }
+    else if (cardChoice1 === `${card21.name}`) {
+        return card21
+    }
+    else if (cardChoice1 === `${card22.name}`) {
+        return card22
+    }
+    else if (cardChoice1 === `${card23.name}`) {
+        return card23
+    }
+    else if (cardChoice1 === `${card24.name}`) {
+        return card24
+    }
+    else if (cardChoice1 === `${card25.name}`) {
+        return card25
+    }
+    else if (cardChoice1 === `${card26.name}`) {
+        return card26
+    }
+    else if (cardChoice1 === `${card27.name}`) {
+        return card27
+    }
+    else if (cardChoice1 === `${card28.name}`) {
+        return card28
+    }
+    else if (cardChoice1 === `${card29.name}`) {
+        return card29
+    }
+    else if (cardChoice1 === `${card30.name}`) {
+        return card30
+    }
+}
+
+const pickCardTwo = () => {
+    let cardChoice2 = prompt("pick your second card")
+    if (cardChoice2 === `${card1.name}`) {
+        return card1
+    }
+    else if (cardChoice2 === `${card2.name}`) {
+        return card2
+    }
+    else if (cardChoice2 === `${card3.name}`) {
+        return card3
+    }
+    else if (cardChoice2 === `${card4.name}`) {
+        return card4
+    }
+    else if (cardChoice2 === `${card5.name}`) {
+        return card5
+    }
+    else if (cardChoice2 === `${card6.name}`) {
+        return card6
+    }
+    else if (cardChoice2 === `${card7.name}`) {
+        return card7
+    }
+    else if (cardChoice2 === `${card8.name}`) {
+        return card8
+    }
+    else if (cardChoice2 === `${card9.name}`) {
+        return card9
+    }
+    else if (cardChoice2 === `${card10.name}`) {
+        return card10
+    }
+    else if (cardChoice2 === `${card11.name}`) {
+        return card11
+    }
+    else if (cardChoice2 === `${card12.name}`) {
+        return card12
+    }
+    else if (cardChoice2 === `${card13.name}`) {
+        return card13
+    }
+    else if (cardChoice2 === `${card14.name}`) {
+        return card14
+    }
+    else if (cardChoice2 === `${card15.name}`) {
+        return card15
+    }
+    else if (cardChoice2 === `${card16.name}`) {
+        return card16
+    }
+    else if (cardChoice2 === `${card17.name}`) {
+        return card17
+    }
+    else if (cardChoice2 === `${card18.name}`) {
+        return card18
+    }
+    else if (cardChoice2 === `${card19.name}`) {
+        return card19
+    }
+    else if (cardChoice2 === `${card20.name}`) {
+        return card20
+    }
+    else if (cardChoice2 === `${card21.name}`) {
+        return card21
+    }
+    else if (cardChoice2 === `${card22.name}`) {
+        return card22
+    }
+    else if (cardChoice2 === `${card23.name}`) {
+        return card23
+    }
+    else if (cardChoice2 === `${card24.name}`) {
+        return card24
+    }
+    else if (cardChoice2 === `${card25.name}`) {
+        return card25
+    }
+    else if (cardChoice2 === `${card26.name}`) {
+        return card26
+    }
+    else if (cardChoice2 === `${card27.name}`) {
+        return card27
+    }
+    else if (cardChoice2 === `${card28.name}`) {
+        return card28
+    }
+    else if (cardChoice2 === `${card29.name}`) {
+        return card29
+    }
+    else if (cardChoice2 === `${card30.name}`) {
+        return card30
+    }
+}
+
+const compareCards = (firstCard, secondCard) => {
+    if (firstCard.strength > secondCard.strength) {
+        console.log(`The winner is ${firstCard.name}`)
+    }
+    else if (secondCard.strength > firstCard.strength) {
+        console.log(`The winner is ${secondCard.name}`)
+    }
+    else {
+        console.log("It's a tie")
+    }
+}
+
+const each_round = () => {
+    let firstCard = pickCardOne()
+    let secondCard = pickCardTwo()
+    console.log(firstCard)
+    console.log(secondCard)
+    compareCards(firstCard, secondCard)
+}
+
+each_round()
+
+
+

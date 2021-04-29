fdf3612d8bd7e236ed23fed28db53e2d0b6f48d7
Kim <faeriekim3@gmail.com>
2021-04-29 15:51:31 +0100

    two player game non-repeating.  Also made code more elegant.

--- script.js
+++ script.js
@@ -52,98 +52,13 @@ let player2Hand = []
 const pickCardOne = () => {
     console.log(player1Hand)
     let cardChoice1 = prompt("Player 1: pick your card.")
-    if (cardChoice1 === `${card1.name}`) {
-        return card1
-    }
-    else if (cardChoice1 === `${card2.name}`) {
-        return card2
-    }
-    else if (cardChoice1 === `${card3.name}`) {
-        return card3
-    }
-    else if (cardChoice1 === `${card4.name}`) {
-        return card4
-    }
-    else if (cardChoice1 === `${card5.name}`) {
-        return card5
-    }
-    else if (cardChoice1 === `${card6.name}`) {
-        return card6
-    }
-    else if (cardChoice1 === `${card7.name}`) {
-        return card7
-    }
-    else if (cardChoice1 === `${card8.name}`) {
-        return card8
-    }
-    else if (cardChoice1 === `${card9.name}`) {
-        return card9
-    }
-    else if (cardChoice1 === `${card10.name}`) {
-        return card10
-    }
-    else if (cardChoice1 === `${card11.name}`) {
-        return card11
-    }
-    else if (cardChoice1 === `${card12.name}`) {
-        return card12
-    }
-    else if (cardChoice1 === `${card13.name}`) {
-        return card13
-    }
-    else if (cardChoice1 === `${card14.name}`) {
-        return card14
-    }
-    else if (cardChoice1 === `${card15.name}`) {
-        return card15
-    }
-    else if (cardChoice1 === `${card16.name}`) {
-        return card16
-    }
-    else if (cardChoice1 === `${card17.name}`) {
-        return card17
-    }
-    else if (cardChoice1 === `${card18.name}`) {
-        return card18
-    }
-    else if (cardChoice1 === `${card19.name}`) {
-        return card19
-    }
-    else if (cardChoice1 === `${card20.name}`) {
-        return card20
-    }
-    else if (cardChoice1 === `${card21.name}`) {
-        return card21
-    }
-    else if (cardChoice1 === `${card22.name}`) {
-        return card22
-    }
-    else if (cardChoice1 === `${card23.name}`) {
-        return card23
-    }
-    else if (cardChoice1 === `${card24.name}`) {
-        return card24
-    }
-    else if (cardChoice1 === `${card25.name}`) {
-        return card25
-    }
-    else if (cardChoice1 === `${card26.name}`) {
-        return card26
-    }
-    else if (cardChoice1 === `${card27.name}`) {
-        return card27
-    }
-    else if (cardChoice1 === `${card28.name}`) {
-        return card28
-    }
-    else if (cardChoice1 === `${card29.name}`) {
-        return card29
-    }
-    else if (cardChoice1 === `${card30.name}`) {
-        return card30
-    }
-    else {
-        return "does not exist"
+    let x;
+    for (x in player1Hand) {
+        if (cardChoice1 === `${player1Hand[x].name}` && x < 15) {
+            return player1Hand[x];
+            x ++;
+        }
+        
     }
 }
 
@@ -151,98 +66,13 @@ const pickCardOne = () => {
 const pickCardTwo = () => {
     console.log(player2Hand)
     let cardChoice2 = prompt("Player 2.  Pick your card.")
-    if (cardChoice2 === `${card1.name}`) {
-        return card1
-    }
-    else if (cardChoice2 === `${card2.name}`) {
-        return card2
-    }
-    else if (cardChoice2 === `${card3.name}`) {
-        return card3
-    }
-    else if (cardChoice2 === `${card4.name}`) {
-        return card4
-    }
-    else if (cardChoice2 === `${card5.name}`) {
-        return card5
-    }
-    else if (cardChoice2 === `${card6.name}`) {
-        return card6
-    }
-    else if (cardChoice2 === `${card7.name}`) {
-        return card7
-    }
-    else if (cardChoice2 === `${card8.name}`) {
-        return card8
-    }
-    else if (cardChoice2 === `${card9.name}`) {
-        return card9
-    }
-    else if (cardChoice2 === `${card10.name}`) {
-        return card10
-    }
-    else if (cardChoice2 === `${card11.name}`) {
-        return card11
-    }
-    else if (cardChoice2 === `${card12.name}`) {
-        return card12
-    }
-    else if (cardChoice2 === `${card13.name}`) {
-        return card13
-    }
-    else if (cardChoice2 === `${card14.name}`) {
-        return card14
-    }
-    else if (cardChoice2 === `${card15.name}`) {
-        return card15
-    }
-    else if (cardChoice2 === `${card16.name}`) {
-        return card16
-    }
-    else if (cardChoice2 === `${card17.name}`) {
-        return card17
-    }
-    else if (cardChoice2 === `${card18.name}`) {
-        return card18
-    }
-    else if (cardChoice2 === `${card19.name}`) {
-        return card19
-    }
-    else if (cardChoice2 === `${card20.name}`) {
-        return card20
-    }
-    else if (cardChoice2 === `${card21.name}`) {
-        return card21
-    }
-    else if (cardChoice2 === `${card22.name}`) {
-        return card22
-    }
-    else if (cardChoice2 === `${card23.name}`) {
-        return card23
-    }
-    else if (cardChoice2 === `${card24.name}`) {
-        return card24
-    }
-    else if (cardChoice2 === `${card25.name}`) {
-        return card25
-    }
-    else if (cardChoice2 === `${card26.name}`) {
-        return card26
-    }
-    else if (cardChoice2 === `${card27.name}`) {
-        return card27
-    }
-    else if (cardChoice2 === `${card28.name}`) {
-        return card28
-    }
-    else if (cardChoice2 === `${card29.name}`) {
-        return card29
-    }
-    else if (cardChoice2 === `${card30.name}`) {
-        return card30
-    }
-    else {
-        return "does not exist"
+    let x = 0;
+    for (x in player2Hand) {
+        if (cardChoice2 === `${player2Hand[x].name}` && x < 15) {
+            return player2Hand[x];
+            x ++;
+        }
+        
     }
 }
 
@@ -325,21 +155,25 @@ const compareCards = (firstCard, secondCard, choice) => {
     }
 }
 
+
+
 // this happens each round
 const each_round = () => {
     let choice = prompt("Please pick a statistic to compare: (strength, agility, intelligence, spells, corruption).")
     if (choice === "strength" || choice === "agility" || choice === "intelligence" || choice === "spells" || choice === "corruption") {
-        let firstCard = pickCardOne();
-        let secondCard = pickCardTwo();
-        if (firstCard === "does not exist" || secondCard === "does not exist") {
-            console.log("Sorry but one or both of those cards does not exist.  Please choose from the available trump cards.")
-            each_round()
+        let firstCard = pickCardOne()
+        while (firstCard === undefined) {
+            console.log("Sorry.  You do not have that card.  Try again.");
+            firstCard = pickCardOne()
         }
-        else {
-            console.log(firstCard)
-            console.log(secondCard)
-            compareCards(firstCard, secondCard, choice)
+        let secondCard = pickCardTwo()
+        while (secondCard === undefined) {
+            console.log("Sorry.  You do not have that card.  Try again.");
+            secondCard = pickCardTwo()
         }
+        console.log(firstCard)
+        console.log(secondCard)
+        compareCards(firstCard, secondCard, choice)
     }
     else {
         console.log("I'm sorry but that statistic doesn't exist.  Please try again.");
